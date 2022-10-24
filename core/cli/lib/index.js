ce43305bdb9116828feb455bee4fafafff5e3c0e
sjfewo <245476427@qq.com>
2022-10-24 22:49:25 +0800

    node支持最低版本

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -7,11 +7,30 @@ module.exports = core;
 // .json -> JSON.parse
 // .node -> process.node
 // any -> .js
-const pkg = require('../package.json');
+const semver = require('semver');
+const colors = require('colors/safe')
 const log = require('@tdd-cli-dev/log');
+const pkg = require('../package.json');
+const constant = require('./const');
 
 function core() {
-    checkPkgVersion();
+   try {
+       checkPkgVersion();
+       checkNodeVersion();
+   } catch (e) {
+       log.error(e.message)
+   }
+}
+
+// 最低支持版本号
+function checkNodeVersion() {
+    // 获取当前Node版本号
+    const currentVersion = process.version;
+    // 对比最低版本号
+    const lowestVersion = constant.LOWEST_NODE_VERSION;
+    if (!semver.gte(currentVersion,lowestVersion)) {
+        throw new Error(colors.red(`tdd-cli 需要安装 v${lowestVersion} 以上版本的 Node.js`))
+    }
 }
 
 // 检查版本号
