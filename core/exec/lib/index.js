b55be9303aff627f83c16cc710dc0374ae5b28ea
sjfewo <245476427@qq.com>
2022-10-30 20:12:39 +0800

    添加package模块

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -1,8 +1,15 @@
 'use strict';
 
-module.exports = exec;
+const Package = require('@tdd-cli-dev/package');
 
 function exec() {
+    const pkg = new Package();
+    console.log(pkg);
     console.log('exec');
     console.log(process.env.CLI_TARGET_PATH)
+    // 1. targetPath -> modulePath
+    // 2. modulePath -> Package(npm模块)
+    // 3. Package.getRootFile(获取入口文件)
 }
+
+module.exports = exec;
---
+++ models/package/README.md
@@ -0,0 +1,11 @@
+# `@tdd-cli-dev/package`
+
+> TODO: description
+
+## Usage
+
+```
+const package = require('@tdd-cli-dev/package');
+
+// TODO: DEMONSTRATE API
+```
