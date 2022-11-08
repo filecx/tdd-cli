9757b34b9954f3dd60622fc8f13a9a3397fa6fd8
sjfewo <245476427@qq.com>
2022-11-08 22:09:54 +0800

    项目名称和版本号的交互

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -3,15 +3,20 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const fse = require('fs-extra');
+const semver = require('semver');
 const Command = require('@tdd-cli-dev/command');
 const log = require('@tdd-cli-dev/log');
+const {eq} = require("semver");
+
+const TYPE_PROJECT = 'project';
+const TYPE_COMPONENT = 'component';
 
 class InitCommand extends Command {
     init() {
         this.projectName = this._argv[0] || '';
         this.force = !!this._cmd.force;
-        log.verbose('projectName', this.projectName);
-        log.verbose('force', this.force);
+        // log.verbose('projectName', this.projectName);
+        // log.verbose('force', this.force);
     }
     async exec() {
       try {
@@ -41,6 +46,7 @@ class InitCommand extends Command {
                     return;
                 }
             }
+            // 2.是否启动强制更新
             if (ifContinue || this.force) {
                 // 给用户二次确认
                 const  {confirmDelete} = await inquirer.prompt({
@@ -55,11 +61,80 @@ class InitCommand extends Command {
                 // 清空当前目录
             }
         }
-        // const localPath = process.cwd();
-        // console.log(localPath);
-        // 2.是否启动强制更新
+        return await this.getProjectInfo()
+    }
+
+    async getProjectInfo() {
+        const projectInfo = {};
         // 3.选择创建项目或组件
+        const {type} = await inquirer.prompt({
+            type: 'list',
+            name: 'type',
+            message: '请选择初始化类型',
+            default: TYPE_PROJECT,
+            choices: [{
+                name: '项目',
+                value: TYPE_PROJECT
+            }, {
+                name: '组件',
+                value: TYPE_COMPONENT
+            }]
+        });
+        log.verbose('type', type);
         // 4.获取项目的基本信息
+        if (type === TYPE_PROJECT) {
+            const o = await inquirer.prompt([{
+                type: 'input',
+                name: 'projectName',
+                message: '请输入项目名称',
+                default: '',
+                validate: function (v) {
+                    const done = this.async();
+                    setTimeout(function (){
+                        //1.输入的首字符必须为英文字符
+                        //2.尾子符必须为英文或数字, 不能为字符
+                        //3.字符仅允许"-_"
+                        //\w=a-zA-Z0-9_
+                        if (!/^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(v)) {
+                            done('请输入合法的项目名称');
+                            return;
+                        }
+                        done(null, true);
+                    },0)
+
+
+                    // return /^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(v);
+                }
+            }, {
+                type: 'input',
+                name: 'projectVersion',
+                message: '请输入项目版本号',
+                default: '1.0.0',
+                validate:function (v) {
+                    const done = this.async();
+                    setTimeout(function (){
+                        if (!(!!semver.valid(v))) {
+                            done('请输入合法的版本号');
+                            return;
+                        }
+                        done(null, true);
+                    },0)
+                },
+                filter: function (v) {
+                    if (semver.valid(v)) {
+                        return semver.valid(v);
+                    } else {
+                        return v;
+                    }
+
+                }
+            }]);
+            console.log(o)
+        } else if (type === TYPE_COMPONENT){
+
+        }
+
+        return projectInfo;
     }
 
     isDirEmpty(localPath) {
